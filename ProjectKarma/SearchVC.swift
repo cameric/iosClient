14bdd42829c00d2a9eedcdda16a0ffd6ffcf9cdb
zhzhang2012 <zhzhangtony@gmail.com>
2015-11-30 02:31:54 -0800

    [Tony] Project structure changed

--- ProjectKarma/SearchVC.swift
+++ ProjectKarma/SearchVC.swift
@@ -9,19 +9,15 @@
 import UIKit
 import Foundation
 
-<<<<<<< HEAD:ProjectKarma/SearchVC.swift
 class SearchVC: UITableViewController {
+
     @IBOutlet var searchBar_: UISearchBar!
     @IBOutlet var tableView_: UITableView!
     
-=======
-class SearchVC: UIViewController {
->>>>>>> parent of d45d8be... init:ProjectKarma/Search/SearchVC.swift
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        AVObject testObject = AVObject.
+                
     }
     
     override func didReceiveMemoryWarning() {
